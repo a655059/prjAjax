d855f67e17e459a14358faea9990877c89b816cb
ShawnShen <a655059@hotmail.com.tw>
2022-10-03 11:41:34 +0800

    加入專案檔案。

---
+++ prjAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prjAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
